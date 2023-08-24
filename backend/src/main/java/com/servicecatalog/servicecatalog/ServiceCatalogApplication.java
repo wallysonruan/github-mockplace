4fe5ae6fda8cad5581a020052a9d5369099e89e8
wallyson ruan <wallysonruan@gmail.com>
2023-08-24 18:59:57 -0300

    INITIG BACKEND

---
+++ backend/src/main/java/com/servicecatalog/servicecatalog/ServiceCatalogApplication.java
@@ -0,0 +1,13 @@
+package com.servicecatalog.servicecatalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceCatalogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceCatalogApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/servicecatalog/servicecatalog/ServiceCatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.servicecatalog.servicecatalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceCatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
