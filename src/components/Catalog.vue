38c402ae2118fae1280749b1ec5e2580da5c23ad
wallyson ruan <wallysonruan@gmail.com>
2023-08-24 13:30:52 -0300

    WIP: Separating Catalog and Product

---
+++ src/components/Catalog.vue
@@ -0,0 +1,89 @@
+<script setup lang="ts">
+import { ProductItem } from './Product.vue';
+import Product from './Product.vue';
+
+
+const products: ProductItem[] = [
+    {
+        title: "Hacking Abilities",
+        text: "Get infinity gossip information!"
+    },
+    {
+        title: "Love",
+        text: "Be loved for eternity!"
+    },
+    {
+        title: "Self-steem",
+        text: "Be the most self-steemed person in the Universe."
+    }
+]
+
+const benefits: ProductItem[] = [
+    {
+        title: "Cheap",
+        text: "Pay using dollar, the prices are in Real."
+    },
+    {
+        title: "Anonimity",
+        text: "No one cares about what you do on the web."
+    }
+]
+
+</script>
+
+<template>
+        <div class="container">
+            <div class="catalog__menu">
+                <div>
+                    <h2>Types</h2>
+                    <ul>
+                        <li>
+                            <a href="#">Apps</a>
+                            <a href="#">Actions</a>
+                        </li>
+                    </ul>
+                </div>
+                <div>
+                    <h2>Categories</h2>
+                    <ul>
+                        <li>
+                            <a href="#">AWS EKS</a>
+                            <a href="#">S3</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <div class="catalog__products-container">
+                <section>
+                    <h2>Products</h2>
+                    <hr>
+                    <Product :products="products"/>
+                </section>
+                <section>
+                    <h2>Benefits</h2>
+                    <hr>
+                    <Product :products="benefits"/>
+                </section>
+            </div>
+        </div>
+</template>
+
+<style scoped>
+    .container{
+        display: flex;
+        flex-direction: row;
+    }
+
+    .container div{
+        background-color: blue;
+    }
+
+    .catalog__menu{
+        flex-grow: 1;
+    }
+
+    .catalog__products-container{
+        flex-grow: 4;
+        background-color: yellow;
+    }
+</style>
---
+++ src/components/Product.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+
+export type ProductItem =  {
+    title: string;
+    text: string;
+};
+
+
+defineProps<{ 
+    products: ProductItem[],
+}>()
+
+</script>
+
+<template>
+    <div class="product__container">
+        <div class="container__item" v-for="product in products" :key="product.title.slice(0,3)">
+            <h3> {{product.title}}</h3>
+            <p> {{product.text}} </p>
+            <a href="#">Learn More</a>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+.product__container{
+    display: flex;
+    flex-direction: row;
+    justify-content: space-around;
+}
+
+.container__item{
+    width: 10rem;
+    background-color: brown;
+}
+</style>
