cfc2293d15aa6a66d114af7efa07b0d86075507d
wallyson ruan <wallysonruan@gmail.com>
2023-08-24 16:16:15 -0300

    FEAT: Added vue-router, and first router

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,7 @@
-<script setup lang="ts">
-import NavBar from './components/NavBar.vue'
-import Catalog from './components/Catalog.vue';
-
-</script>
-
 <template>
-    <NavBar />
-    <Catalog />
+    <div class="section">
+        <div class="container">
+            <RouterView />
+        </div>
+    </div>
 </template>
-
