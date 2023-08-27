0e33fe80739cf405187d71510eafe86f32cd22f4
wallyson ruan <wallysonruan@gmail.com>
2023-08-26 21:55:24 -0300

    WIP: Building 'awards' verification in Product

--- frontend/src/components/Product.vue
+++ frontend/src/components/Product.vue
@@ -23,8 +23,22 @@ defineProps<{
     <div class="custom-card border-0">
         <div>
             <h3 class="custom-card-title">
-                    {{product.title}}
-                </h3>
+                {{product.title}}
+            </h3>
+            <div class="publisher-container">
+                <p class="publisher-name">
+                    <span id="boot-icon" class="bi bi-patch-check custom-patch-check"></span>
+                    <small>
+                        By {{ product.publisher_name }}
+                    </small>
+                </p>
+                <p class="publisher-is-verified" v-if="product.is_publisher_verified">
+                    <span id="boot-icon" class="bi bi-patch-check custom-patch-check"></span>
+                    <small>
+                        Publisher domain and email verified
+                    </small>
+                </p>
+            </div>
                 <p class="custom-card-text"> {{product.description}} </p>
             </div>
         </div>
@@ -39,12 +53,21 @@ a{
     height: 100%;
 }
 .custom-card-title{
+    color: #0969da;
     font-size: large;
     font-weight: 500;
-    color: #0969da;
+    margin: 0;
 }
 .custom-card-text{
     color: #656D76;
     font-size: medium;
+}
+.publisher-container{
+    display: flex;
+    color: #656D76;
+}
+.custom-patch-check{
+    color: #0969da;
+    margin-right: 0.5rem;
 }
 </style>
