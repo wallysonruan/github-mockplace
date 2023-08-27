f683ca0c8189665cb9b87d1456f059416d2582f9
wallyson ruan <wallysonruan@gmail.com>
2023-08-27 03:33:21 -0300

    WIP: Added Recommended tag

--- frontend/src/components/Product.vue
+++ frontend/src/components/Product.vue
@@ -39,7 +39,12 @@ defineProps<{
                     </small>
                 </p>
             </div>
-                <p class="custom-card-text"> {{product.description}} </p>
+                <p class="custom-card-text">
+                    {{product.description}}
+                </p>
+                <span v-if="product.is_recommended" class="is_recommended">
+                    Recommended
+                </span>
             </div>
         </div>
     </a>
@@ -51,6 +56,7 @@ a{
 }
 .custom-card{
     height: 100%;
+    margin-bottom: 1rem;
 }
 .custom-card-title{
     color: #0969da;
@@ -60,14 +66,29 @@ a{
 }
 .custom-card-text{
     color: #656D76;
-    font-size: medium;
+    font-size: 0.9rem;
+    margin: 0;
 }
 .publisher-container{
     display: flex;
     color: #656D76;
+    font-size: small;
 }
 .custom-patch-check{
     color: #0969da;
-    margin-right: 0.5rem;
+    margin-right: 0.1rem;
+}
+
+.publisher-name{
+    margin-right: 1ch;
+}
+
+.is_recommended{
+    font-weight: 600;
+    font-size: 12px;
+    color: #656D76;
+    border: 0.3px solid #656D76;
+    border-radius: 10%/60%;
+    padding: 3px 7px;
 }
 </style>
