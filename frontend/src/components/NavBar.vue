9baf74cab21ab98f530239f9136dc2a5f23c180d
wallyson ruan <wallysonruan@gmail.com>
2023-09-05 16:46:53 -0300

    WIP: Adding floating navbar menu

--- frontend/src/components/NavBar.vue
+++ frontend/src/components/NavBar.vue
@@ -1,84 +1,95 @@
 <script setup lang="ts">
+import { Ref, ref } from 'vue';
+import NavBarMenu from './NavBarMenu.vue';
+
+const menuFlag: Ref<boolean> = ref<boolean>(true);
+
+function showMenu(){
+    menuFlag.value = menuFlag.value ? false : true;
+}
 
 </script>
 
 <template>
-    <nav class="custom-navbar">
-        <div class="left">
-            <!--Hamburguer menu-->
-            <button type="button">
-                <svg xmlns="http://www.w3.org/2000/svg" width="15" height="25" fill="currentColor" class="button__icon" viewBox="0 0 16 16">
-                    <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"/>
-                </svg>
-            </button>
-
-            <a href="#" class="brand__logo">
-                <img src="../assets/images/logo-github.png" alt="Github" width="32" height="32">
-            </a>
-
-            <span class="brand__name">
-                Mockplace
-            </span>
-        </div>
-
-        <div class="right">
-            <div class="search-bar__container">
-                <button type="button" class="search-bar__bar">
-                    <i class="bi bi-search search-bar__search-icon"></i>
-                    <span class="search-bar__text no-medium-view">
-                        Type <kbd>/</kbd> to search
-                    </span>
-                </button>
-
-                <span class="decorative-pipe search-bar__decorative-pipe no-medium-view"></span>
-
-                <button type="button" class="button__command no-medium-view">
-                    <svg fill="#656D76" aria-hidden="true" height="16" viewBox="0 0 16 16" version="1.1" width="16" data-view-component="true" class="octicon octicon-command-palette">
-                        <path d="m6.354 8.04-4.773 4.773a.75.75 0 1 0 1.061 1.06L7.945 8.57a.75.75 0 0 0 0-1.06L2.642 2.206a.75.75 0 0 0-1.06 1.061L6.353 8.04ZM8.75 11.5a.75.75 0 0 0 0 1.5h5.5a.75.75 0 0 0 0-1.5h-5.5Z"></path>
+    <div>
+        <nav class="custom-navbar">
+            <div class="left">
+                <!--Hamburguer menu-->
+                <button type="button" @click="showMenu">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="15" height="25" fill="currentColor" class="button__icon" viewBox="0 0 16 16">
+                        <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"/>
                     </svg>
                 </button>
+
+                <a href="#" class="brand__logo">
+                    <img src="../assets/images/logo-github.png" alt="Github" width="32" height="32">
+                </a>
+
+                <span class="brand__name">
+                    Mockplace
+                </span>
             </div>
 
-            <span class="decorative-pipe navbar__decorative-pipe"></span>
+            <div class="right">
+                <div class="search-bar__container">
+                    <button type="button" class="search-bar__bar">
+                        <i class="bi bi-search search-bar__search-icon"></i>
+                        <span class="search-bar__text no-medium-view">
+                            Type <kbd>/</kbd> to search
+                        </span>
+                    </button>
 
-            <!--Create new... icon-->
-            <button type="button" class="button__create-new">
-                <!--Plus icon-->
-                <svg fill="#656D76" aria-hidden="true" height="16" viewBox="0 0 16 16" version="1.1" width="16" data-view-component="true" class="octicon octicon-plus">
-                    <path d="M7.75 2a.75.75 0 0 1 .75.75V7h4.25a.75.75 0 0 1 0 1.5H8.5v4.25a.75.75 0 0 1-1.5 0V8.5H2.75a.75.75 0 0 1 0-1.5H7V2.75A.75.75 0 0 1 7.75 2Z"></path>
-                </svg>
-                <!--Arrow down icon-->
-                <svg fill="#656D76" aria-hidden="true" height="16" viewBox="0 0 16 16" version="1.1" width="16" data-view-component="true" class="octicon octicon-triangle-down">
-                    <path d="m4.427 7.427 3.396 3.396a.25.25 0 0 0 .354 0l3.396-3.396A.25.25 0 0 0 11.396 7H4.604a.25.25 0 0 0-.177.427Z"></path>
-                </svg>
-            </button>
+                    <span class="decorative-pipe search-bar__decorative-pipe no-medium-view"></span>
 
-            <!--Issues icon-->
-            <button type="button">
-                <svg fill="#656D76" aria-hidden="true" height="16" viewBox="0 0 16 16" version="1.1" width="16" data-view-component="true" class="octicon octicon-issue-opened Button-visual">
-                    <path d="M8 9.5a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3Z"></path><path d="M8 0a8 8 0 1 1 0 16A8 8 0 0 1 8 0ZM1.5 8a6.5 6.5 0 1 0 13 0 6.5 6.5 0 0 0-13 0Z"></path>
-                </svg>
-            </button>
+                    <button type="button" class="button__command no-medium-view">
+                        <svg fill="#656D76" aria-hidden="true" height="16" viewBox="0 0 16 16" version="1.1" width="16" data-view-component="true" class="octicon octicon-command-palette">
+                            <path d="m6.354 8.04-4.773 4.773a.75.75 0 1 0 1.061 1.06L7.945 8.57a.75.75 0 0 0 0-1.06L2.642 2.206a.75.75 0 0 0-1.06 1.061L6.353 8.04ZM8.75 11.5a.75.75 0 0 0 0 1.5h5.5a.75.75 0 0 0 0-1.5h-5.5Z"></path>
+                        </svg>
+                    </button>
+                </div>
 
-            <!--Pull requests icon-->
-            <button type="button">
-                <svg fill="#656D76" aria-hidden="true" height="16" viewBox="0 0 16 16" version="1.1" width="16" data-view-component="true" class="octicon octicon-git-pull-request Button-visual">
-                    <path d="M1.5 3.25a2.25 2.25 0 1 1 3 2.122v5.256a2.251 2.251 0 1 1-1.5 0V5.372A2.25 2.25 0 0 1 1.5 3.25Zm5.677-.177L9.573.677A.25.25 0 0 1 10 .854V2.5h1A2.5 2.5 0 0 1 13.5 5v5.628a2.251 2.251 0 1 1-1.5 0V5a1 1 0 0 0-1-1h-1v1.646a.25.25 0 0 1-.427.177L7.177 3.427a.25.25 0 0 1 0-.354ZM3.75 2.5a.75.75 0 1 0 0 1.5.75.75 0 0 0 0-1.5Zm0 9.5a.75.75 0 1 0 0 1.5.75.75 0 0 0 0-1.5Zm8.25.75a.75.75 0 1 0 1.5 0 .75.75 0 0 0-1.5 0Z"></path>
-                </svg>
-            </button>
+                <span class="decorative-pipe navbar__decorative-pipe"></span>
 
-            <!--Inbox icon-->
-            <button type="button">
-                <svg fill="#656D76" aria-hidden="true" height="16" viewBox="0 0 16 16" version="1.1" width="16" data-view-component="true" class="octicon octicon-inbox color-fg-muted mr-0">
-                    <path d="M2.8 2.06A1.75 1.75 0 0 1 4.41 1h7.18c.7 0 1.333.417 1.61 1.06l2.74 6.395c.04.093.06.194.06.295v4.5A1.75 1.75 0 0 1 14.25 15H1.75A1.75 1.75 0 0 1 0 13.25v-4.5c0-.101.02-.202.06-.295Zm1.61.44a.25.25 0 0 0-.23.152L1.887 8H4.75a.75.75 0 0 1 .6.3L6.625 10h2.75l1.275-1.7a.75.75 0 0 1 .6-.3h2.863L11.82 2.652a.25.25 0 0 0-.23-.152Zm10.09 7h-2.875l-1.275 1.7a.75.75 0 0 1-.6.3h-3.5a.75.75 0 0 1-.6-.3L4.375 9.5H1.5v3.75c0 .138.112.25.25.25h12.5a.25.25 0 0 0 .25-.25Z"></path>
-                </svg>
-            </button>
+                <!--Create new... icon-->
+                <button type="button" class="button__create-new">
+                    <!--Plus icon-->
+                    <svg fill="#656D76" aria-hidden="true" height="16" viewBox="0 0 16 16" version="1.1" width="16" data-view-component="true" class="octicon octicon-plus">
+                        <path d="M7.75 2a.75.75 0 0 1 .75.75V7h4.25a.75.75 0 0 1 0 1.5H8.5v4.25a.75.75 0 0 1-1.5 0V8.5H2.75a.75.75 0 0 1 0-1.5H7V2.75A.75.75 0 0 1 7.75 2Z"></path>
+                    </svg>
+                    <!--Arrow down icon-->
+                    <svg fill="#656D76" aria-hidden="true" height="16" viewBox="0 0 16 16" version="1.1" width="16" data-view-component="true" class="octicon octicon-triangle-down">
+                        <path d="m4.427 7.427 3.396 3.396a.25.25 0 0 0 .354 0l3.396-3.396A.25.25 0 0 0 11.396 7H4.604a.25.25 0 0 0-.177.427Z"></path>
+                    </svg>
+                </button>
 
-            <button type="button" class="button__profile">
-                <img src="../assets/images/profile-avatar.png" alt="" size="32" width="32" height="32">
-            </button>
-        </div>
-    </nav>
+                <!--Issues icon-->
+                <button type="button">
+                    <svg fill="#656D76" aria-hidden="true" height="16" viewBox="0 0 16 16" version="1.1" width="16" data-view-component="true" class="octicon octicon-issue-opened Button-visual">
+                        <path d="M8 9.5a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3Z"></path><path d="M8 0a8 8 0 1 1 0 16A8 8 0 0 1 8 0ZM1.5 8a6.5 6.5 0 1 0 13 0 6.5 6.5 0 0 0-13 0Z"></path>
+                    </svg>
+                </button>
+
+                <!--Pull requests icon-->
+                <button type="button">
+                    <svg fill="#656D76" aria-hidden="true" height="16" viewBox="0 0 16 16" version="1.1" width="16" data-view-component="true" class="octicon octicon-git-pull-request Button-visual">
+                        <path d="M1.5 3.25a2.25 2.25 0 1 1 3 2.122v5.256a2.251 2.251 0 1 1-1.5 0V5.372A2.25 2.25 0 0 1 1.5 3.25Zm5.677-.177L9.573.677A.25.25 0 0 1 10 .854V2.5h1A2.5 2.5 0 0 1 13.5 5v5.628a2.251 2.251 0 1 1-1.5 0V5a1 1 0 0 0-1-1h-1v1.646a.25.25 0 0 1-.427.177L7.177 3.427a.25.25 0 0 1 0-.354ZM3.75 2.5a.75.75 0 1 0 0 1.5.75.75 0 0 0 0-1.5Zm0 9.5a.75.75 0 1 0 0 1.5.75.75 0 0 0 0-1.5Zm8.25.75a.75.75 0 1 0 1.5 0 .75.75 0 0 0-1.5 0Z"></path>
+                    </svg>
+                </button>
+
+                <!--Inbox icon-->
+                <button type="button">
+                    <svg fill="#656D76" aria-hidden="true" height="16" viewBox="0 0 16 16" version="1.1" width="16" data-view-component="true" class="octicon octicon-inbox color-fg-muted mr-0">
+                        <path d="M2.8 2.06A1.75 1.75 0 0 1 4.41 1h7.18c.7 0 1.333.417 1.61 1.06l2.74 6.395c.04.093.06.194.06.295v4.5A1.75 1.75 0 0 1 14.25 15H1.75A1.75 1.75 0 0 1 0 13.25v-4.5c0-.101.02-.202.06-.295Zm1.61.44a.25.25 0 0 0-.23.152L1.887 8H4.75a.75.75 0 0 1 .6.3L6.625 10h2.75l1.275-1.7a.75.75 0 0 1 .6-.3h2.863L11.82 2.652a.25.25 0 0 0-.23-.152Zm10.09 7h-2.875l-1.275 1.7a.75.75 0 0 1-.6.3h-3.5a.75.75 0 0 1-.6-.3L4.375 9.5H1.5v3.75c0 .138.112.25.25.25h12.5a.25.25 0 0 0 .25-.25Z"></path>
+                    </svg>
+                </button>
+
+                <button type="button" class="button__profile">
+                    <img src="../assets/images/profile-avatar.png" alt="" size="32" width="32" height="32">
+                </button>
+            </div>
+        </nav>
+        <NavBarMenu v-if="menuFlag"/>
+    </div>
 </template>
 
 <style scoped>
---
+++ frontend/src/components/NavBarMenu.vue
@@ -0,0 +1,179 @@
+<script setup lang="ts">
+
+type option = {
+    title: string,
+    url: string
+}
+
+const options: option[] = [
+    {
+        title: "Home",
+        url: "dashboard"
+    },
+    {
+        title: "Issues",
+        url: "Issues"
+    },
+    {
+        title: "Pull requests",
+        url: "pulls"
+    },
+    {
+        title: "Discussions",
+        url: "discussions"
+    },
+    ]
+
+const repositories: string[] = [
+    'wallysonruanTW/real-state-prices',
+    'tecnologia-acessivel/programa-inclua',
+    'wallysonruanTW/timesheetCalculator',
+    'wallysonruanTW/gilded-rose-kata',
+    'wallysonruanTW/Tennis-Refactoring'
+]
+
+const teams: string[] = [
+    'tecnologia-acessivel/ta-atuacaointerna'
+]
+
+const footerOptions: string[] = [
+    'Explore',
+    'Mockplace',
+    'Give new navigation feedback'
+]
+
+const footerMoreOptions: string[] = [
+    'About',
+    'Blog',
+    'Terms',
+    'Privacy',
+    'Security',
+    'Status'
+]
+</script>
+
+<template>
+    <aside class="menu-container">
+        <div class="top">
+            <div>
+                <div class="brand__logo">
+
+                </div>
+                <div class="button__close">
+                    <button type="button">
+                        
+                    </button>
+                </div>
+            </div>
+            <nav>
+                <ul>
+                    <li v-for="option in options" :key="option.title">
+                        <a :href="option.url">
+                            {{ option.title }}
+                        </a>
+                    </li>
+                </ul>
+            </nav>
+            <hr>
+            <section>
+                <div class="section-header">
+                    <span>Repositories</span>
+                    <i class="bi bi-search search-bar__search-icon"></i>
+                </div>
+                <nav>
+                    <ul>
+                        <li v-for="repository in repositories" :key="repository">
+                            <a :href="repository">
+                                {{ repository }}
+                            </a>
+                        </li>
+                    </ul>
+                </nav>
+                <button type="button">Show more</button>
+            </section>
+            <hr>
+            <section>
+                <div class="section-header">
+                    <span>Teams</span>
+                    <i class="bi bi-search search-bar__search-icon"></i>
+                </div>
+                <nav>
+                    <ul>
+                        <li v-for="team in teams" :key="team">
+                            <a :href="team">
+                                {{ team }}
+                            </a>
+                        </li>
+                    </ul>
+                </nav>
+            </section>
+            <hr>
+        </div>
+        <!---->
+        <footer>
+            <div>
+                <li v-for="option in footerOptions" :key="option">
+                    <a :href="option">
+                        {{ option }}
+                    </a>
+                </li>
+            </div>
+            <div>
+                <span>
+                    @ 2023 Mockplace, Inc.
+                </span>
+                <ul>
+                    <li v-for="option in footerMoreOptions" :key="option">
+                        <a :href="option">
+                            {{ option }}
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </footer>
+    </aside>
+</template>
+
+<style scoped>
+
+a, a:visited{
+    text-decoration: none;
+    color: black;
+}
+
+li{
+    list-style: none;
+}
+
+.menu-container{
+    position: absolute;
+    z-index: 2;
+
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+
+    height: 100vh;
+    padding: 1rem;
+
+    background-color: white;
+    border-top-right-radius: 20px;
+    border-bottom-right-radius: 20px;
+
+    top: 0%;
+    left: 0%;
+}
+
+.top{
+
+}
+
+section{
+    margin-bottom: 1rem;
+}
+
+.section-header{
+    display: flex;
+    justify-content: space-between;
+}
+</style>
